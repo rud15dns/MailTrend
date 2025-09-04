133525012132719776eb3c22ef0e9c5df56c21cd
Son Hyunbin <147061193+SickofU@users.noreply.github.com>
2025-09-04 15:17:19 +0900

    feat: s3 설정 주입
    # Conflicts:
    #	.env
    #	.gitignore
    #	src/main/resources/application.properties

--- src/main/java/com/example/mailtrend/MailSend/config/S3Config.java
+++ src/main/java/com/example/mailtrend/MailSend/config/S3Config.java
@@ -1,4 +1,40 @@
 package com.example.mailtrend.MailSend.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
 public class S3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.storage}")
+    private String storage;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(region)
+                .build();
+    }
 }
